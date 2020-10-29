6bbe3cd816cbd17eb3e5cf1bb444c06358a98892
danielbrogan26 <72405192+danielbrogan26@users.noreply.github.com>
2020-10-29 09:54:05 -0700

    Add files via upload

---
+++ functionDemo.py
@@ -0,0 +1,13 @@
+def printStars(n):
+	print(n, " ", end="")
+	print("* * * * * * * * * * * * *")
+	print("* * * * * * * * * * * * *")
+	print("* * * * * * * * * * * * *")
+	
+def main():
+	printStars(1)# calling a function printStars()
+	printStars(2)
+	printStars(3)
+
+main()
+	
